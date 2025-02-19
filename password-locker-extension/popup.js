fe142668857428aee2a8b248ba26a8fe384676a6
Bharath387 <lingabharath387@gmail.com>
2025-02-19 17:12:59 +0530

    modified show password list

--- password-locker-extension/popup.js
+++ password-locker-extension/popup.js
@@ -1,8 +1,11 @@
 document.getElementById('savePassword').addEventListener('click', savePassword);
 document.getElementById('showPassword').addEventListener('click', showPassword);
 document.getElementById('generatePassword').addEventListener('click', generatePassword);
+document.getElementById('copyPassword').addEventListener('click', copyFunction);
 
 const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,16}$/; // A simple pattern for password validation
+
+let isShowPassVisibled = false;
 
 function savePassword() {
   const pageURL = document.getElementById('pageURL').value;
@@ -25,23 +28,36 @@ function savePassword() {
 
   chrome.storage.local.set({ [pageURL]: credentials }, function () {
     alert('Password saved successfully!');
+    if(isShowPassVisibled){
+      showPassword();
+    }
   });
 }
 
 function showPassword() {
   const pageURL = document.getElementById('pageURL').value;
   const adminId = document.getElementById('adminId').value;
+  const ul = document.getElementById('storedData');
+  ul.innerHTML='';
   console.log(adminId);
   if (adminId == 123456) {
-    chrome.storage.local.get(pageURL, function (data) {
-      if (data[pageURL]) {
-        const decryptedPassword = decryptPassword(data[pageURL].password);
-        document.getElementById('storedUsername').innerText =  data[pageURL].username;
-        document.getElementById('storedPassword').innerText = decryptedPassword;
-      } else {
-        alert('No details stored for this URL.');
-      }
-    });
+    chrome.storage.local.getKeys(function (data) {
+      data.forEach(url => {
+        chrome.storage.local.get(url, function (data) {
+          if (data[url]) {
+            const decryptedPassword = decryptPassword(data[url].password);
+            let li = document.createElement('li');
+            li.innerHTML = `username:${data[url].username}<br>
+             password: ${decryptedPassword}<br>
+             url: ${data[url].url}`;
+            ul.appendChild(li);
+            isShowPassVisibled = true;
+          } else {
+            alert('No details stored for this URL.');
+          }
+        });
+      });
+    })
   } else {
     alert('Not valid user id' + adminId);
   }
@@ -54,7 +70,7 @@ function generatePassword() {
   const length = lengthCheckbox ? getRandomInt(8, 16) : 8; // Default length is 8
   const password = generateRandomPassword(length, specialCheckbox);
 
-  document.getElementById('generatedPassword').innerText = password;
+  document.getElementById('generatedPassword').value = password;
 }
 
 function generateRandomPassword(length, includeSpecial) {
@@ -86,3 +102,28 @@ function encryptPassword(password) {
 function decryptPassword(encryptedPassword) {
   return atob(encryptedPassword);
 }
+
+function copyFunction() {
+  // Get the text field
+  var copyText = document.getElementById("generatedPassword");
+  if (copyText.value == '') {
+    alert("Please generate password before copy");
+    return;
+  }
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+  navigator.clipboard.writeText(copyText.value);
+  // Alert the copied text
+  alert("Copied the text: " + copyText.value);
+}
+
+function fillCurrentPageUrl() {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    // Get the URL of the active tab
+    document.getElementById('pageURL').value = tabs[0].url;
+  });
+
+}
+
+fillCurrentPageUrl();
